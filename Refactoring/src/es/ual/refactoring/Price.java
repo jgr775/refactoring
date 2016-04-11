4cfb3d28d53d4a0fb904999cafc00fb21b70e66e
Jose Omar <joseomargalafatrdguez@gmail.com>
2016-04-11 14:11:40 +0200

    HERENCIA
    En cada tipo de película el método getCharge varía, por lo que podríamos
    aprovechar esto para modificar el switch haciendo uso de polimorfismo.
    Sin embargo esto no es posible ya que una película debe de poder cambiar
    su calsificación (puede pasar de new release a regular o children por lo
    que se modificaría su precio) pero un objeto no puede cambiar su clase
    en ejecución. Para ello utlizaremos el patrón de diseño State,
    consiguiendo que cada película tenga un precio variable mediante el uso
    de una clase abstracta Price y tres que heredan de esta: RegularPrice,
    ChildrensPrice y NewReleasePrice.
    Para ello llevaremos a cabo tres refactorizaciones:
    - Reemplazar tipos con estados: modificamos el atributo _priceCode
    encapsulándolo para asegurar que solo se usa a través de los métodos
    getter y setter. Creamos la clase abstracta Price y sus hijos, y
    modificamos el tipo de _priceCode a Price.
    -Aplicamos la técnica de mover método a getCharge y lo pasamos de Movie
    a Price. 
    - Por último ya podemos llevar a cabo la técnica reemplazar condicional
    con polimorfismo: pasamos del switch de getCharge a implementar este
    método en cada uno de los hijos, dejando el método original como un
    método abstracto.
    
    Haremos lo msimo con el método getFrequentRenterPoints, moviendo el
    método de la calse Rental a la clase Price e implementando el método en
    la clase NewReleasePrice. EN este caso el método original de Price lo
    dejamos implementado, no como abstracto.

---
+++ Refactoring/src/es/ual/refactoring/Price.java
@@ -0,0 +1,11 @@
+package es.ual.refactoring;
+
+public abstract class Price {
+	public abstract int getPriceCode();
+
+	public abstract double getCharge(int daysRented);
+
+	public int getFrequentRenterPoints(int daysRented) {
+		return 1;
+	}
+}
---
+++ Refactoring/src/es/ual/refactoring/RegularPrice.java
@@ -0,0 +1,15 @@
+package es.ual.refactoring;
+
+public class RegularPrice extends Price {
+	public int getPriceCode() {
+		return Movie.REGULAR;
+	}
+
+	@Override
+	public double getCharge(int daysRented) {
+		double result = 2;
+		if (daysRented > 2)
+			result += (daysRented - 2) * 1.5;
+		return result;
+	}
+}
