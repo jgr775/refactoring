3b4ebc66730111a1b78059712d0c8ea12160f18b
Jose Omar <joseomargalafatrdguez@gmail.com>
2016-04-11 12:35:57 +0200

    Gracias al paso anterior de reemplazar varias variables temporales con métodos ahora podemos crear el método htmlStatement de forma sencilla reusando estos métodos creados. Así conseguimos que un cambio en la lógica de la aplicación sea llevado a cabo de forma rápida y sin afectar a código que debería de ser independiente. Creado nuevo test para comprobar el método htmlStatement.

--- Refactoring/test/es/ual/refactoring/test/Test.java
+++ Refactoring/test/es/ual/refactoring/test/Test.java
@@ -3,24 +3,38 @@ package es.ual.refactoring.test;
 import es.ual.refactoring.*;
 import static org.junit.Assert.*;
 
-public class Test {
+import org.junit.Before;
 
-	@org.junit.Test
-	public void test() {
+public class Test {
+	private Customer yo;
+
+	@Before
+	public void setUp() {
 		Movie movie1 = new Movie("Star Wars VII The Force Awakens", 1);
 		Movie movie2 = new Movie("The Imitation Game", 0);
 
-		Customer yo = new Customer("Jose Omar");
+		yo = new Customer("Jose Omar");
 		Rental r1 = new Rental(movie1, 10);
 		Rental r2 = new Rental(movie2, 2);
 
 		yo.addRental(r1);
 		yo.addRental(r2);
+	}
 
+	@org.junit.Test
+	public void statemenTest() {
 		String res = "Rental Record for " + "Jose Omar" + "\n" + "\tStar Wars VII The Force Awakens\t30.0" + "\n"
 				+ "\tThe Imitation Game\t2.0" + "\n" + "Amount owed is 32.0" + "\n"
 				+ "You earned 3 frequent renter points";
 
 		assertEquals(res, yo.statement());
+	}
+
+	@org.junit.Test
+	public void htmlStatementTest() {
+		String html = "<H1>Rentals for <EM>Jose Omar</EM></H1><P>" + "\n" + "Star Wars VII The Force Awakens: 30.0<BR>"
+				+ "\n" + "The Imitation Game: 2.0<BR>" + "\n" + "<P>You owe <EM>32.0</EM><P>" + "\n"
+				+ "On this rental you earned <EM>3</EM> frequent renter points<P>";
+		assertEquals(html, yo.htmlStatement());
 	}
 }
